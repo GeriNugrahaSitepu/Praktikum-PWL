e3ba2963ff56627690320f07fd41775a78322101
GeriNugrahaSitepu <geri.n.sitepu@gmail.com>
2024-04-11 00:51:26 +0700

    first commit

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\dosen;
 
 class DosenController extends Controller
 {
@@ -11,7 +12,10 @@ class DosenController extends Controller
      */
     public function index()
     {
-        return view('dosen');
+        $dosen = dosen::all();
+        return view('dosen', [
+            'dosen' => $dosen
+        ]);
     }
 
     /**
--- app/Models/ktm.php
+++ app/Models/ktm.php
@@ -5,12 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ktm extends Model
+class Ktm extends Model
 {
     use HasFactory;
     protected $table = 'ktm';
     protected $primaryKey = 'id_ktm';
+
     public function mahasiswa(){
         return $this->belongsTo(Mahasiswa::class);
     }
+
+    public function dosen(){
+        return $this->belongsTo(dosen::class);
+    }
+
 }
